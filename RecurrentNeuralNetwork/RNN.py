1ec68b286662ebf7704f75399336b7405484256d
KocT9H <55079150+KocT9H@users.noreply.github.com>
2019-09-10 12:46:39 +0300

    Implement RNN class
    Improve architecture.  No passing data between methods required

--- RecurrentNeuralNetwork/RNN.py
+++ RecurrentNeuralNetwork/RNN.py
@@ -11,53 +11,115 @@ introtodeeplearning.com
 
 """
 
-import tensorflow as tf 
+import tensorflow as tf
 import numpy as np
 import os
 import functools
 import util
 
 
-def GenerateData():
-    tempStr = ''
-    for i in range(0,10000):
-        tempStr += 'kostya'
-    with open('myData.txt', 'a') as fileObj:
-        fileObj.write(tempStr)
+class RNN:
+    singleInputLen = 100
+    embeddingDimension = 256
+    rnnUnits = 1024
+    batchSize = 64
+    bufferSize = 10000
+    epochsNum = 1
 
-    
-def GetDataFromFile():
-    with open('myData.txt', 'r') as fileObj:
-        fileStr = fileObj.read()
-        print(f'Length of text: {len(fileStr)} characters\n')
-        print(f'First chars: {fileStr[:250]}\n')
-        return fileStr
+    def __init__(self, vocabSize, dataAsInt):
+        tf.enable_eager_execution()
+        self.model = None
+        self.LSTM = None
+        self.dataSet = None
+        self.checkpoint_dir = ''
+        self.vocabSize = vocabSize
+        self.dataAsInt = dataAsInt
+        self.optimizer = tf.train.AdamOptimizer()
+        self.CreateDataSet()
+        self.DefineRNNModel()
+
+    def CreateDataSet(self):
+        # Convert the text vector into a stream of character indices
+        charDataset = tf.data.Dataset.from_tensor_slices(self.dataAsInt)
+
+        # Convert this stream of character indices to sequences of the desired size
+        sequences = charDataset.batch(RNN.singleInputLen + 1, drop_remainder=True)
+
+        # Define the input and target texts for each sequence
+        self.dataSet = sequences.map(SplitInputTarget)
+
+        # Shuffle the data for the purpose of stochastic gradient descent
+        # Pack it into batches which will be used during training
+        self.dataSet = self.dataSet.shuffle(self.bufferSize).batch(RNN.batchSize, drop_remainder=True)
+
+    def DefineRNNModel(self):
+        self.LSTM = functools.partial(tf.keras.layers.LSTM, recurrent_activation='sigmoid')
+        self.LSTM = functools.partial(self.LSTM,
+                                      return_sequences=True,
+                                      recurrent_initializer='glorot_uniform',
+                                      stateful=True)
+        self.BuildModel(RNN.batchSize)
+        print('RNN Model summary:')
+        print(self.model.summary())
+
+    def BuildModel(self, batchSize):
+        self.model = tf.keras.Sequential(
+            [tf.keras.layers.Embedding(self.vocabSize, RNN.embeddingDimension, batch_input_shape=[batchSize, None]),
+             self.LSTM(RNN.rnnUnits),
+             tf.keras.layers.Dense(self.vocabSize)])
+
+    def TrainRNNModel(self):
+        self.checkpoint_dir = './training_checkpoints'
+        checkpoint_prefix = os.path.join(self.checkpoint_dir, "ckpt_{epoch}")
+        history = []
+        plotter = util.PeriodicPlotter(sec=1, xlabel='Iterations', ylabel='Loss')
+        for epoch in range(RNN.epochsNum):
+            self.model.reset_states()
+            for inp, target in self.dataSet:
+                with tf.GradientTape() as tape:
+                    predictions = self.model(inp)
+                    loss = ComputeLoss(target, predictions)
+
+                # Compute the gradients and try to minimize
+                grads = tape.gradient(loss, self.model.trainable_variables)
+                self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+
+                # Update the progress bar!
+                history.append(loss.numpy().mean())
+                plotter.plot(history)
+
+            # Update the model with the changed weights!
+            self.model.save_weights(checkpoint_prefix.format(epoch=epoch))
+        print(f'Final loss %: {loss.numpy().mean()}')
+
+    def RebuildModelWithDifferentBatch(self, newBatch):
+        self.BuildModel(newBatch)
+        self.model.load_weights(tf.train.latest_checkpoint(self.checkpoint_dir))
+        self.model.build(tf.TensorShape([newBatch, None]))
+
+    def PredictText(self, start_string, char2idx, idx2char, generation_length=1000):
+        # Evaluation step (generating text using the learned RNN model)
+        input_eval = [char2idx[s] for s in start_string]
+        input_eval = tf.expand_dims(input_eval, 0)
+
+        self.model.reset_states()
+        text_generated = []
+
+        for i in range(generation_length):
+            predictions = self.model(input_eval)
+            # Remove the batch dimension
+            predictions = tf.squeeze(predictions, 0)
+            predicted_id = tf.multinomial(predictions, num_samples=1)[-1, 0].numpy()
+            # Pass the prediction along with the previous hidden state
+            # as the next inputs to the model
+            input_eval = tf.expand_dims([predicted_id], 0)
+            text_generated.append(idx2char[predicted_id])
+
+        return start_string + ''.join(text_generated)
 
 
-def CreateCharIntDB(fileStr) :
-    vocabulary = sorted(set(fileStr))
-    char2idx = {u:i for i, u in enumerate(vocabulary)}
-    idx2char = np.array(vocabulary)
-    dataAsInt = np.array([char2idx[c] for c in fileStr])
-    
-    print(f'File has {len(vocabulary)} unique characters\n')
-    return [vocabulary, char2idx, idx2char, dataAsInt]
-    
-
-def CreateTrainingExamples(dataLen, dataAsInt, singleInputLen, BATCH_SIZE, BUFFER_SIZE):
-    # Convert the text vector into a stream of character indices
-    charDataset = tf.data.Dataset.from_tensor_slices(dataAsInt)
-    
-    # Convert this stream of character indices to sequences of the desired size
-    sequences = charDataset.batch(singleInputLen+1, drop_remainder=True)
-    
-    # Define the input and target texts for each sequence
-    dataSet = sequences.map(SplitInputTarget)
-
-    # Shuffle the data for the purpose of stochastic gradient descent
-    # Pack it into batches which will be used during training
-    dataSet = dataSet.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
-    return dataSet
+def ComputeLoss(labels, logits):
+    return tf.keras.backend.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
 
 def SplitInputTarget(chunk):
@@ -66,127 +128,41 @@ def SplitInputTarget(chunk):
     return inputText, targetText
 
 
-def DefineRNNModel(vocabSize, embeddingDimension, rnnUnits, batchSize):
-    LSTM = functools.partial(tf.keras.layers.LSTM, recurrent_activation='sigmoid')
-    LSTM = functools.partial(LSTM, 
-                             return_sequences=True, 
-                             recurrent_initializer='glorot_uniform',
-                             stateful=True)
-    model = BuildModel(LSTM,
-                       vocabSize, 
-                       embeddingDimension, 
-                       rnnUnits, 
-                       batchSize)
-    print('RNN Model summary:')
-    print(model.summary())
-    return [model, LSTM]
+def GenerateData():
+    tempStr = ''
+    for i in range(0, 10000):
+        tempStr += 'kostya'
+    with open('myData.txt', 'a') as fileObj:
+        fileObj.write(tempStr)
 
 
-def BuildModel(LSTM, vocabSize, embeddingDim, rnnUnits, batchSize):
-    model = tf.keras.Sequential([
-            tf.keras.layers.Embedding(vocabSize, embeddingDim, 
-                                      batch_input_shape=[batchSize, None]),
-                                      LSTM(rnnUnits),
-                                      tf.keras.layers.Dense(vocabSize)])
-    return model
-    
-
-def TrainRNNModel(model, dataSet, EPOCHS_NUM):
-    optimizer = tf.train.AdamOptimizer()
-    checkpoint_dir = './training_checkpoints'
-    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
-    
-    history = []
-    plotter = util.PeriodicPlotter(sec=1, xlabel='Iterations', ylabel='Loss')
-    for epoch in range(EPOCHS_NUM):
-        # Initialize the hidden state at the start of every epoch; initially is None
-        hidden = model.reset_states()
-        
-        # Enumerate the dataset for use in training
-        for inp, target in dataSet:
-            with tf.GradientTape() as tape:
-                predictions = model(inp)
-                loss = ComputeLoss(target, predictions)
-
-            # Compute the gradients and try to minimize
-            grads = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(grads, model.trainable_variables))
-            
-            # Update the progress bar!
-            history.append(loss.numpy().mean())
-            plotter.plot(history)
-        
-        # Update the model with the changed weights!
-        model.save_weights(checkpoint_prefix.format(epoch=epoch))
-
-    print(f'Final loss %: {loss.numpy().mean()}')
-    return checkpoint_dir
+def GetDataFromFile():
+    with open('myData.txt', 'r') as fileObj:
+        fileStr = fileObj.read()
+        print(f'Length of text: {len(fileStr)} characters\n')
+        print(f'First chars: {fileStr[:250]}\n')
+        return fileStr
 
 
-def ComputeLoss(labels, logits):
-    return tf.keras.backend.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+def CreateCharIntDB(fileStr):
+    vocabulary = sorted(set(fileStr))
+    char2idx = {u: i for i, u in enumerate(vocabulary)}
+    idx2char = np.array(vocabulary)
+    dataAsInt = np.array([char2idx[c] for c in fileStr])
 
-
-def RebuildModelWithDifferentBatch(LSTM, vocabSize, embeddingDim, rnnUnits, batchSize, checkpoint_dir):
-    model = BuildModel(LSTM, vocabSize, embeddingDim, rnnUnits, batchSize)
-    model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
-    model.build(tf.TensorShape([1, None]))
-    return model
-    
-    
-def PredictText(model, start_string, char2idx, idx2char, generation_length=1000):
-    # Evaluation step (generating text using the learned RNN model)
-    input_eval = [char2idx[s] for s in start_string]
-    input_eval = tf.expand_dims(input_eval, 0)
-
-    text_generated = []
-
-    model.reset_states()
-    for i in range(generation_length):
-        predictions = model(input_eval)
-      
-        # Remove the batch dimension
-        predictions = tf.squeeze(predictions, 0)
-      
-        predicted_id = tf.multinomial(predictions, num_samples=1)[-1,0].numpy()
-      
-        # Pass the prediction along with the previous hidden state
-        # as the next inputs to the model
-        input_eval = tf.expand_dims([predicted_id], 0)
-      
-        text_generated.append(idx2char[predicted_id])
-
-    return start_string + ''.join(text_generated)
+    print(f'File has {len(vocabulary)} unique characters\n')
+    return [vocabulary, char2idx, idx2char, dataAsInt]
 
 
 def main():
-    singleInputLen = 100
-    embeddingDimension = 256
-    rnnUnits = 1024
-    BATCH_SIZE = 64
-    BUFFER_SIZE = 10000
-    EPOCHS_NUM = 2
-    
-    #GenerateData()
-    
+    # GenerateData()
     fileStr = GetDataFromFile()
-    
     [vocabulary, char2idx, idx2char, dataAsInt] = CreateCharIntDB(fileStr)
-    
-    dataSet = CreateTrainingExamples(len(fileStr), dataAsInt, singleInputLen, BATCH_SIZE, BUFFER_SIZE)
-    
-    [model, LSTM] = DefineRNNModel(len(vocabulary), embeddingDimension, rnnUnits, BATCH_SIZE)
-    
-    checkpoint_dir = TrainRNNModel(model, dataSet, EPOCHS_NUM)
-    
-    model = RebuildModelWithDifferentBatch(LSTM,
-                                           len(vocabulary), 
-                                           embeddingDimension, 
-                                           rnnUnits, 
-                                           1, 
-                                           checkpoint_dir)
-    
-    text = PredictText(model, 'k', char2idx, idx2char)
+
+    rnnInstance = RNN(len(vocabulary), dataAsInt)
+    rnnInstance.TrainRNNModel()
+    rnnInstance.RebuildModelWithDifferentBatch(1)
+    text = rnnInstance.PredictText('k', char2idx, idx2char)
     print(f'Predicted text:\n{text}')
 
 
--- RecurrentNeuralNetwork/util.py
+++ RecurrentNeuralNetwork/util.py
@@ -7,58 +7,59 @@ introtodeeplearning.com
 
 """
 
-from IPython import display as ipythondisplay
+from IPython import display
 import matplotlib.pyplot as plt
 import time
 from string import Formatter
-import tensorflow as tf 
+import tensorflow as tf
 import numpy as np
-    
+
+
 class PeriodicPlotter:
-  def __init__(self, sec, xlabel='', ylabel='', scale=None):
+    def __init__(self, sec, xlabel='', ylabel='', scale=None):
 
-    self.xlabel = xlabel
-    self.ylabel = ylabel
-    self.sec = sec
-    self.scale = scale
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+        self.sec = sec
+        self.scale = scale
 
-    self.tic = time.time()
+        self.tic = time.time()
 
-  def plot(self, data):
-    if time.time() - self.tic > self.sec:
-      plt.cla()
-      
-      if self.scale is None:
-        plt.plot(data)
-      elif self.scale == 'semilogx':
-        plt.semilogx(data)
-      elif self.scale == 'semilogy':
-        plt.semilogy(data)
-      elif self.scale == 'loglog':
-        plt.loglog(data)
-      else:
-        raise ValueError("unrecognized parameter scale {}".format(self.scale))
+    def plot(self, data):
+        if time.time() - self.tic > self.sec:
+            plt.cla()
 
-      plt.xlabel(self.xlabel); plt.ylabel(self.ylabel)
-      ipythondisplay.clear_output(wait=True)
-      ipythondisplay.display(plt.gcf())
+            if self.scale is None:
+                plt.plot(data)
+            elif self.scale == 'semilogx':
+                plt.semilogx(data)
+            elif self.scale == 'semilogy':
+                plt.semilogy(data)
+            elif self.scale == 'loglog':
+                plt.loglog(data)
+            else:
+                raise ValueError("unrecognized parameter scale {}".format(self.scale))
 
-      self.tic = time.time()
-      
+            plt.xlabel(self.xlabel);
+            plt.ylabel(self.ylabel)
+            display.clear_output(wait=True)
+            display.display(plt.gcf())
+
+            self.tic = time.time()
+
 
 def display_model(model):
-  tf.keras.utils.plot_model(model,
-             to_file='tmp.png',
-             show_shapes=True)
-  from IPython.display import Image
-  return Image('tmp.png')
+    tf.keras.utils.plot_model(model,
+                              to_file='tmp.png',
+                              show_shapes=True)
+    return display.Image('tmp.png')
 
 
-def plot_sample(x,y,vae):
-    plt.figure(figsize=(2,1))
+def plot_sample(x, y, vae):
+    plt.figure(figsize=(2, 1))
     plt.subplot(1, 2, 1)
 
-    idx = np.where(y.numpy()==1)[0][0]
+    idx = np.where(y.numpy() == 1)[0][0]
     plt.imshow(x[idx])
     plt.grid(False)
 
@@ -70,11 +71,12 @@ def plot_sample(x,y,vae):
 
 
 class LossHistory:
-  def __init__(self, smoothing_factor=0.0):
-    self.alpha = smoothing_factor
-    self.loss = []
-  def append(self, value):
-    self.loss.append( self.alpha*self.loss[-1] + (1-self.alpha)*value if len(self.loss)>0 else value )
-  def get(self):
-    return self.loss
+    def __init__(self, smoothing_factor=0.0):
+        self.alpha = smoothing_factor
+        self.loss = []
 
+    def append(self, value):
+        self.loss.append(self.alpha * self.loss[-1] + (1 - self.alpha) * value if len(self.loss) > 0 else value)
+
+    def get(self):
+        return self.loss
