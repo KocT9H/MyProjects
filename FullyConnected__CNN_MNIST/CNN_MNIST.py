8c6af1237780d73d900010f829d9a9a4fd5da1dd
KocT9H <55079150+KocT9H@users.noreply.github.com>
2019-09-12 10:03:17 +0300

    Add view predictions code

--- FullyConnected__CNN_MNIST/CNN_MNIST.py
+++ FullyConnected__CNN_MNIST/CNN_MNIST.py
@@ -3,19 +3,16 @@
 """
 Example of Fully Connected Neural Network
 Classify numbers according to MNIST data set
-
 @author: Konstantin Verein
 @Email:  koct9h@gmail.com
-
 Based on MIT course:
 © MIT 6.S191: Introduction to Deep Learning
 introtodeeplearning.com
-
 """
 
 import tensorflow as tf
 import numpy as np
-
+import matplotlib.pyplot as plt
 tf.enable_eager_execution()
 
 # Get MNIST DB
@@ -27,6 +24,7 @@ test_images = np.expand_dims(test_images, axis=-1) / 255.
 test_labels = np.int64(test_labels)
 
 
+# Create CNN model
 def build_cnn_model():
     cnnModel = tf.keras.Sequential([
         tf.keras.layers.Conv2D(filters=24, kernel_size=(3, 3), input_shape=(28, 28, 1), activation=tf.nn.relu),
@@ -40,10 +38,8 @@ def build_cnn_model():
     return cnnModel
 
 
-# Create CNN model
 cnn_model = build_cnn_model()
 print(cnn_model.summary())
-
 cnn_model.compile(optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.1),
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
@@ -51,8 +47,39 @@ cnn_model.compile(optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.1)
 # Define the batch size and the number of epochs to use during training
 batchSize = 64
 epochs = 5
-
+# Train model
 cnn_model.fit(train_images, train_labels, batch_size=batchSize, epochs=epochs)
 
+# Evaluate accuracy
 test_loss, test_acc = cnn_model.evaluate(test_images, test_labels)
 print('Test accuracy:', test_acc)
+
+
+# View predictions
+def plot_image(imgNum, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[imgNum], true_label[imgNum], img[imgNum]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(np.squeeze(img), cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(predicted_label, 100 * np.max(predictions_array), true_label),
+               color=color)
+
+
+predictions = cnn_model.predict(test_images)
+num_rows = 5
+num_cols = 4
+num_images = num_rows * num_cols
+plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+for imgNum in range(num_images):
+    plt.subplot(num_rows, 2 * num_cols, 2 * imgNum + 1)
+    plot_image(imgNum, predictions, test_labels, test_images)
+    
